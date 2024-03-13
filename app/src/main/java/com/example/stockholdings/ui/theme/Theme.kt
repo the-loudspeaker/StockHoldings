7f5e8c6bc53b4c67bad01a8d84eaeae98a46c8a9
Raman Sarda <raman@supaki.io>
2024-03-13 16:13:24 +0530

    feat: Update colorscheme.

--- app/src/main/java/com/example/stockholdings/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stockholdings/ui/theme/Theme.kt
@@ -17,9 +17,9 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = Color(0xFF800080),
+    secondary = Color(0xFFCCC2DC),
+    tertiary = Color(0xFF800080),
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -30,9 +30,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = Color(0xFF800080),
+    secondary = Color(0xFFCCC2DC),
+    tertiary = Color(0xFF800080),
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
